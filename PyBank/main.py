342d46c4e6718c0c10487b3b42e3b8e34521908d
Gabe Stuhr <66698826+gfstuhr@users.noreply.github.com>
2020-07-02 15:45:38 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,6 +15,11 @@ PyBank_data = os.path.join("Resources", "budget_data.csv")
 with open(PyBank_data, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvreader)
+
+    for row in csvreader:
+        #Month Count
+        Month_Count.append(row[0])
+        print(Month_Count)
+        #revenue
+        Revenue.append(row[1])
     
-
-
